53d1b392ca20d8742624a95f0f976de244fc6b58
JeGalSoo <jgs0318@gmail.com>
2024-05-07 20:10:27 +0900

    240507 ssr

--- soccer-server/front/nextjs-12-example-with-typescript-and-styled-components-main/src/pages/Stadium/index.tsx
+++ soccer-server/front/nextjs-12-example-with-typescript-and-styled-components-main/src/pages/Stadium/index.tsx
@@ -4,22 +4,29 @@ import {
     QueryClientProvider,
     useQuery,
 } from '@tanstack/react-query'
-import StadiumList from "app/stadium/module/stadium-list";
+import StadiumList from "app/api/stadium/module/stadium-list";
+import { useEffect } from "react";
+import { getStadiums, getStadiumsKey } from "app/api/stadium/service/stadium-service";
 
 
 const StadiumPage:NextPage = ({data} : any) => {
+    useEffect(()=>{
+        console.log('MY-INFO : StadiumPage useEffect data is')
+        console.log(JSON.stringify(data));
+    },[])
     return <StadiumList/>;
 }
 
 export async function getServerSideProps(){
 
     const queryClient = new QueryClient();
-    const result = await fetch('https://api.github.com/repos/TanStack/query')
+    await queryClient.prefetchInfiniteQuery(getStadiumsKey,getStadiums)
+    const data = queryClient.getQueryData(getStadiumsKey);
+    console.log('MY-INFO : StadiumPage SSR data is')
+    console.log(JSON.stringify(data));
+    
     return {
-        props: {
-            data: {data: result},
-        },
-    };
+        props:{data},
+    }
 }
-
 export default StadiumPage;
